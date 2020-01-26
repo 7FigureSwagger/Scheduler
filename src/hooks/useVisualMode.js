d7ad30ae6524fc4823a403f3b63a53d3fd6d9b72
7figureswagger <swistek.labs@gmail.com>
2020-01-26 02:24:47 -0800

    started with custom hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { React, useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  function transition(view, change) {
+    if(!change){
+      setHistory(prev => [...prev, mode]);
+    }
+    setMode(view); 
+  }
+  
+  function back() {
+    if (history.length >= 1) {
+      setMode(history[history.length - 1]);
+      setHistory(prev => [...prev.slice(0, -1)]);
+    }
+  }
+
+  
+  return { mode, transition, back };
+}
+
+// const [history, setHistory] = useState([initial]);
