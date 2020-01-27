74a1af7075111cda761777072eab83efd5c88925
7figureswagger <swistek.labs@gmail.com>
2020-01-27 13:20:32 -0800

    getInterviewersForDay function finished

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,11 +16,24 @@ export const getInterview = (state, interview) => {
  if(!interview){
    return null;
  } else {
-  //  console.log('state', state);
-  //  console.log('interview', interview);
+   
   const student = interview.student;
-  const interviewer = state.interviewers[interview.interviewer];
+  const interviewer = state.appointments.interviewers[interview.interviewer];
   const interviewObj = { student, interviewer };
   return interviewObj;
  }
+};
+
+export const getInterviewersForDay = (state, day) => {
+	const appForDay = state.days
+    .filter(spec => spec.name === day)
+    .map(spec => spec.appointments)
+    .reduce((acc, val) => acc.concat(val), []);
+
+  const appointment = [];
+  appForDay.forEach(spec => {
+    appointment.push(state.appointments[spec]);
+  });
+
+  return appointment;
 };
